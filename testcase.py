3da47982b7a24599fe9d05ad7409eff6f23c71ee
Jim Knoble <jmknoble@pobox.com>
2019-07-16 11:48:49 -0700

    Make test case as minimal as possible

--- testcase.py
+++ testcase.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-
 import os
 import sys
 
 import dummy_thirdparty1
-
-
-def main():
-    print("args: {}".format(sys.argv))
-    print("current dir: {}".format(os.getcwd()))
-    sys.exit(0)
-
-
-if __name__ == "__main__":
-    main()
